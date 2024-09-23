3e1cfb96c692d05a6253e0d1e396cd9025c6706f
caojen <caojen@mail2.sysu.edu.cn>
2024-09-23 22:27:29 +0800

    fix: validate

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,6 @@ use rand::{Rng, thread_rng};
 use rand::distributions::Alphanumeric;
 #[cfg(feature = "serde")]
 use serde::{Deserialize, Serialize};
-use sha2::digest::Update;
 use sha2::{Digest, Sha256};
 
 #[derive(Debug, Eq, PartialEq, Hash)]
@@ -12,6 +11,12 @@ use sha2::{Digest, Sha256};
 pub struct HashedPassword(String);
 
 impl HashedPassword {
+    #[inline]
+    fn separator() -> char {
+        '$'
+    }
+
+    #[inline]
     fn salt() -> String {
         thread_rng()
             .sample_iter(&Alphanumeric)
@@ -44,25 +49,23 @@ impl HashedPassword {
         let row = format!("{}{}{}", salt.as_ref(), plain.as_ref(), hex::encode(secret));
         let hash = Self::hash(&row);
 
-        Self(format!("{}${}", salt, hash))
+        Self(format!("{}{}{}", salt.as_ref(), Self::separator(), hash))
     }
 
     /// To validate `plain` is the plain password or not.
     pub fn validate<T: AsRef<str>, S: AsRef<[u8]>>(&self, plain: T, secret: S) -> bool {
         let plain = plain.as_ref();
-        let mut split = plain.splitn(2, '$');
+        let mut split = self.as_str().splitn(2, '$');
 
-        let (size, _) = split.size_hint();
-        if size != 2 {
-            // invalid hashed password
+        let salt = split.next().unwrap_or_default(); // safe unwrap
+        let hash = split.next().unwrap_or_default(); // safe unwrap
+
+        if salt.is_empty() || hash.is_empty() {
             return false;
         }
 
-        let salt = split.next().unwrap(); // safe unwrap
-        let hash = split.next().unwrap(); // safe unwrap
-
         let result = Self::hash_with_salt(plain, secret, salt);
-        result.0 == hash
+        result.0 == self.as_str()
     }
 
     pub fn as_str(&self) -> &str {
@@ -74,4 +77,19 @@ impl Display for HashedPassword {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         f.write_str(self.as_str())
     }    
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::HashedPassword;
+
+    #[test]
+    fn all() {
+        let secret = b"1234567890";
+        let plain = "abcdefg";
+
+        let password = HashedPassword::from_plain(plain, secret);
+        assert!(password.validate("abcdefg", secret));
+        assert!(!password.validate("abcdef", secret));
+    }
 }
